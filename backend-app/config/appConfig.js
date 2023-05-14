62fb30214a89b6a2c3bb5152511e20eb16d6d981
muttaqin1 <mdmuttaqin20@gmail.com>
2023-05-14 18:40:28 +0600

    Add app config.

---
+++ backend-app/config/appConfig.js
@@ -0,0 +1,8 @@
+const { join } = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: join(__dirname, '../.env') });
+
+exports.logFilePath = join(__dirname, '../server-logs');
+exports.DATABASE = process.env.MONGO_URI;
+exports.DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+exports.PORT = process.env.PORT;
