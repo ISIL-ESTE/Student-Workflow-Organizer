aaa403e154461f1ddf2a8c43538a9ad6fa8bbda9
muttaqin1 <mdmuttaqin20@gmail.com>
2023-06-03 23:51:43 +0600

    Add authorities and restrictions property in user model.

--- backend-app/models/userModel.js
+++ backend-app/models/userModel.js
@@ -1,55 +1,65 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { Authorities, Restrictions } = require('../middlewares/authorization');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please fill your name"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please fill your email"],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, " Please provide a valid email"],
-  },
-  address: {
-    type: String,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please fill your password"],
-    minLength: 6,
-    select: false,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, "Please fill your password confirm"],
-    validate: {
-      validator: function(el) {
-        return el === this.password;
-      },
-      message: "Your password and confirmation password are not the same",
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please fill your name'],
     },
-  },
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user",
-  },
-  active: {
-    type: Boolean,
-    default: true,
-    select: false,
-  },
+    email: {
+      type: String,
+      required: [true, 'Please fill your email'],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, ' Please provide a valid email'],
+    },
+    address: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please fill your password'],
+      minLength: 6,
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please fill your password confirm'],
+      validate: {
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: 'Your password and confirmation password are not the same',
+      },
+    },
+    authorities: {
+      type: String,
+      enum: Object.values(Authorities) || [],
+    },
+    restrictions: {
+      type: String,
+      enum: Object.values(Restrictions) || [],
+    },
+    role: {
+      type: String,
+      enum: ['ADMIN', 'USER', 'SUPER_ADMIN'],
+      default: 'USER',
+    },
+    active: {
+      type: Boolean,
+      default: true,
+      select: false,
+    },
   },
   { timestamps: true }
 );
 
-userSchema.pre("save", async function(next) {
-  if (!this.isModified("password")) {
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
     return next();
   }
 
@@ -59,13 +69,12 @@ userSchema.pre("save", async function(next) {
   next();
 });
 
-userSchema.methods.correctPassword = async function(
+userSchema.methods.correctPassword = async function (
   typedPassword,
-  originalPassword,
+  originalPassword
 ) {
   return await bcrypt.compare(typedPassword, originalPassword);
 };
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 module.exports = User;
-
