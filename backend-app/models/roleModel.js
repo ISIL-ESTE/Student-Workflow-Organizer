6d7dddf5b7b9bcc75952247d3443802e9c2dccff
muttaqin1 <mdmuttaqin20@gmail.com>
2023-06-19 16:57:47 +0600

    Update role model.

--- backend-app/models/roleModel.js
+++ backend-app/models/roleModel.js
@@ -1,28 +1,48 @@
-const { Schema, Model, Types } = require('mongoose');
-
-const roleSchema = Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-    uppercase: true,
+const { Schema, Model } = require('mongoose');
+const { Actions } = require('../middlewares/authorization');
+const validator = require('validator');
+const roleSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      uppercase: true,
+      validate: {
+        validator: (value) => {
+          return value.length > 0;
+        },
+      },
+    },
+    authorities: [
+      {
+        type: String,
+        required: true,
+        validate: {
+          validator: (value) => {
+            return validator.isIn(value, Object.values(Actions));
+          },
+        },
+      },
+    ],
+    authorities: [
+      {
+        type: String,
+        required: true,
+        validate: {
+          validator: (value) => {
+            return validator.isIn(value, Object.values(Actions));
+          },
+        },
+      },
+    ],
   },
-  authorities: [
-    {
-      type: Types.ObjectId,
-      ref: 'Action',
-      required: true,
-    },
-  ],
-  authorities: [
-    {
-      type: Types.ObjectId,
-      ref: 'Action',
-      required: true,
-    },
-  ],
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
-const roleModel = new Model('Role', roleModel);
+const roleModel = new Model('Role', roleSchema);
 module.exports = roleModel;
