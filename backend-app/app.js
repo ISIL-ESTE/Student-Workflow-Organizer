d5c20ded4e04a899c9c9218f2b217b99bb74fa99
Khalid1G <div.khalid1G@gmail.com>
2023-06-22 00:29:24 +0100

    Fix code scanning

--- backend-app/app.js
+++ backend-app/app.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const rateLimit = require('express-rate-limit');
-const compression = require('compression');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
-const cors = require('cors');
-const morgan = require('./middlewares/morgan');
-const swaggerDocs = require('./utils/swagger');
-const {CURRENT_ENV} = require('./config/appConfig');
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const compression = require("compression");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
+const morgan = require("./middlewares/morgan");
+const swaggerDocs = require("./utils/swagger");
+const { CURRENT_ENV } = require("./config/appConfig");
 
-const userRoutes = require('./routes/userRoutes');
-const adminRoutes = require('./routes/adminRoute');
-const globalErrHandler = require('./middlewares/globalErrorHandler');
-const AppError = require('./utils/appError');
+const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoute");
+const globalErrHandler = require("./middlewares/globalErrorHandler");
+const AppError = require("./utils/appError");
 const app = express();
 
 // configure swagger docs
@@ -35,19 +35,23 @@ app.use(helmet());
 const limiter = rateLimit({
   max: 150,
   windowMs: 60 * 60 * 1000,
-  message: 'Too Many Request from this IP, please try again in an hour',
+  message: "Too Many Request from this IP, please try again in an hour",
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
 app.use(
   express.json({
-    limit: '15kb',
+    limit: "15kb",
   })
 );
 
 // Data sanitization against Nosql query injection
-app.use(mongoSanitize());
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 
 // Data sanitization against XSS(clean user input from malicious HTML code)
 app.use(xss());
@@ -59,12 +63,12 @@ app.use(hpp());
 app.use(compression());
 
 // Routes
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/admin', adminRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/admin", adminRoutes);
 
 // handle undefined Routes
-app.use('*', (req, res, next) => {
-  const err = new AppError(404, 'fail', 'Route Not Found', req.path);
+app.use("*", (req, res, next) => {
+  const err = new AppError(404, "fail", "Route Not Found", req.path);
   next(err, req, res, next);
 });
 
