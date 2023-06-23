6e7e32cd5ecb2150467784d1112ba3cca07497ce
muttaqin1 <mdmuttaqin20@gmail.com>
2023-06-23 08:20:27 +0600

    Add utility class auth_utils.

---
+++ backend-app/utils/auth/auth_utils.js
@@ -0,0 +1,51 @@
+const Jwt = require('./jwt');
+const AppError = require('../appError');
+const {
+  TOKEN_ISSUER,
+  TOKEN_AUDIENCE,
+  ACCESS_TOKEN_EXPIRES_IN,
+  REFRESH_TOKEN_EXPIRES_IN,
+} = require('../../config/appConfig');
+
+/**
+ * @description Utility class for authentication
+ * @class AuthUtils
+ * @extends {Jwt}
+ */
+class AuthUtils extends Jwt {
+  /**
+   * Generates Access and Refresh Tokens
+   * @param {object} user
+   * @param {string} accessTokenKey
+   * @param {string} refreshTokenKey
+   * @returns {Promise<{accessToken:string, refreshToken:string}>} tokens
+   */
+  async createTokens(user, accessTokenKey, refreshTokenKey) {
+    try {
+      const accessToken = await this.generateToken(
+        {
+          issuer: TOKEN_ISSUER,
+          audience: TOKEN_AUDIENCE,
+          userId: user._id,
+          accessTokenKey,
+        },
+        ACCESS_TOKEN_EXPIRES_IN
+      );
+      const refreshToken = await this.generateToken(
+        {
+          issuer: TOKEN_ISSUER,
+          audience: TOKEN_AUDIENCE,
+          userId: user._id,
+          refreshTokenKey,
+        },
+        REFRESH_TOKEN_EXPIRES_IN
+      );
+
+      return { accessToken, refreshToken };
+    } catch {
+      throw new AppError(500, 'fail', 'Internal Server Error');
+    }
+  }
+}
+
+module.exports = AuthUtils;
