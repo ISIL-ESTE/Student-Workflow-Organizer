48221c3844c2830ea7a127c882a175461ebfc8f5
muttaqin1 <mdmuttaqin20@gmail.com>
2023-06-23 17:10:03 +0600

    Update globalErrorHandler.

--- backend-app/middlewares/globalErrorHandler.js
+++ backend-app/middlewares/globalErrorHandler.js
@@ -1,5 +1,6 @@
-const httpStatus = require("http-status-codes");
-const currentEnv = process.env.NODE_ENV || "development";
+const httpStatus = require('http-status-codes');
+const currentEnv = process.env.NODE_ENV || 'development';
+const AppError = require('../utils/appError');
 
 /**
  * Error handling middleware
@@ -12,22 +13,48 @@ const currentEnv = process.env.NODE_ENV || "development";
  */
 module.exports = (err, req, res, next) => {
   Logger.error(err.stack);
-  if (err.code === 11000) {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: err.statusCode,
+      title: httpStatus.getStatusText(err.statusCode),
+      details: {
+        ...(err.path && { path: err.path }),
+        description: err.message,
+      },
+      ...(currentEnv === 'development' && {
+        error:
+          ' Do not Forget to remove this in production!            ' +
+          err.stack,
+      }),
+    });
+  } else if (err.code === 11000) {
     Object.keys(err.keyValue).forEach((key) => {
       err.message = `Duplicate value for the field : [${key}] Please use another value!`;
     });
     err.statusCode = 409;
+    return res.status(err.statusCode).json({
+      status: err.statusCode,
+      title: httpStatus.getStatusText(err.statusCode),
+      details: {
+        ...(err.path && { path: err.path }),
+        description: err.message,
+      },
+    });
+  } else {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || 'something went wrong!';
+    return res.status(err.statusCode).json({
+      status: err.statusCode,
+      title: httpStatus.getStatusText(err.statusCode),
+      details: {
+        ...(err.path && { path: err.path }),
+        description: err.message,
+      },
+      ...(currentEnv === 'development' && {
+        error:
+          ' Do not Forget to remove this in production!            ' +
+          err.stack,
+      }),
+    });
   }
-  res.status(err.statusCode).json({
-    status: err.statusCode,
-    title: httpStatus.getStatusText(err.statusCode),
-    details: {
-      ...(err.path && { path: err.path }),
-      description: err.message,
-    },
-    ...(currentEnv === "development" && {
-      error:
-        " Do not Forget to remove this in production!            " + err.stack,
-    }),
-  });
 };
