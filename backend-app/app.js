81ce221b40796375ae9730715315e3eab474ede1
muttaqin1 <mdmuttaqin20@gmail.com>
2023-07-27 18:42:58 +0600

    Pre-commit hook test - lint & fix every .js & .json file.

--- backend-app/app.js
+++ backend-app/app.js
@@ -17,7 +17,6 @@ const app = express();
 // configure swagger docs
 swaggerDocs(app);
 
-
 // use json as default format
 app.use(express.json());
 
@@ -34,16 +33,16 @@ app.use(helmet());
 
 // Body parser, reading data from body into req.body
 app.use(
-  express.json({
-    limit: '15kb',
-  })
+    express.json({
+        limit: '15kb',
+    })
 );
 
 // Data sanitization against Nosql query injection
 app.use(
-  mongoSanitize({
-    replaceWith: '_',
-  })
+    mongoSanitize({
+        replaceWith: '_',
+    })
 );
 
 // Data sanitization against XSS(clean user input from malicious HTML code)
@@ -56,36 +55,35 @@ app.use(hpp());
 app.use(compression());
 
 if (CURRENT_ENV === 'production') {
-  //Limiting request form same IP
-  app.use(limiter);
+    //Limiting request form same IP
+    app.use(limiter);
 }
 
 // check if no version is provided if so use the default version
 // example api/auth/user/signup => api/v1/auth/user/signup
 app.use((req, res, next) => {
-  if (req.originalUrl.startsWith('/api')) {
-    req.originalUrl = `/api/${API_VERSION}${req.originalUrl}`;
-  }
-  console.log("req.originalUrl", req.originalUrl);
-  next();
+    if (req.originalUrl.startsWith('/api')) {
+        req.originalUrl = `/api/${API_VERSION}${req.originalUrl}`;
+    }
+    console.log('req.originalUrl', req.originalUrl);
+    next();
 });
-
 
 // routes
 app.use(`/api/${API_VERSION}`, require('./routes/index'));
 
 app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Welcome to the backend app',
-    env: CURRENT_ENV,
-  });
+    res.status(200).json({
+        status: 'success',
+        message: 'Welcome to the backend app',
+        env: CURRENT_ENV,
+    });
 });
 
 // handle undefined Routes
 app.use('*', (req, res, next) => {
-  const err = new AppError(404, 'fail', 'Route Not Found', req.originalUrl);
-  next(err, req, res, next);
+    const err = new AppError(404, 'fail', 'Route Not Found', req.originalUrl);
+    next(err, req, res, next);
 });
 
 app.use(globalErrHandler);
