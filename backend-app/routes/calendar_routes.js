2772e488e6e8ad424b575a1e7e2fdde41fd40003
Abdelouahab Bella <a.bella4246@uca.ac.ma>
2023-08-12 07:55:45 +0100

    fix - update calendar routes

--- backend-app/routes/calendar_routes.js
+++ backend-app/routes/calendar_routes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const base = require('./base_controller');
 const calendarModel = require('../models/calendar/calendar_model');
+const authController = require('../controllers/auth_controller');
+const calendarController = require('../controllers/calendar_controllers/calendar_base_controller');
 const router = express.Router();
 
 router.post('/', base.createOne(calendarModel))
 router.get('/:id', base.getOne(calendarModel))
 
+router.patch('/:id', base.updateOne(calendarModel))
+router.delete('/:id', base.deleteOne(calendarModel))
+
 router.use(authController.restrictTo('ADMIN', 'SUPER_ADMIN'));
 
 router.get('/', base.getAll(calendarModel))
-
-router.use(authController.restrictTo('SUPER_ADMIN'));
-router.patch('/:id', base.updateOne(calendarModel))
-router.delete('/:id', base.deleteOne(calendarModel))
 
 module.exports = router;
