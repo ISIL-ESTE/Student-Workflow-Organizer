d51611093d1174fa273c40d89f23e3b79576c9ac
Abdelouahab Bella <a.bella4246@uca.ac.ma>
2023-08-15 17:47:23 +0100

    fix - signup conflict

--- backend-app/tests/e2e/auth/signup.test.js
+++ backend-app/tests/e2e/auth/signup.test.js
@@ -2,24 +2,25 @@ const request = require('supertest');
 const app = require('../../../app');
 const { testUserCredentials } = require('../../testConstants');
 const { REQUIRE_ACTIVATION } = require('../../../config/app_config');
-const { deleteUser } = require('../../utils');
+const { createUser, deleteUser } = require('../../utils');
 
 describe('POST /api/auth/signup', () => {
-    it('should create a new user', async () => {
+    beforeAll(async () => {
         await deleteUser(
             app,
             testUserCredentials.userEmail,
             testUserCredentials.userPassword
         );
-        const response = await request(app)
-            .post('/api/auth/signup')
-            .send({
-                name: testUserCredentials.userName,
-                email: testUserCredentials.userEmail,
-                password: testUserCredentials.userPassword,
-            })
-            .expect(201);
+    });
 
+    it('should create a new user', async () => {
+        const response = await createUser(
+            app,
+            testUserCredentials.userName,
+            testUserCredentials.userEmail,
+            testUserCredentials.userPassword
+        );
+        expect(response.status).toBe(201);
         expect(response.body.user).toBeDefined();
         expect(response.body.user.name).toBe(testUserCredentials.userName);
         expect(response.body.user.email).toBe(testUserCredentials.userEmail);
