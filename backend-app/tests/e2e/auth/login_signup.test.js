026fa85a12770f9d483676a8a532f96b13ede747
Abdelouahab Bella <a.bella4246@uca.ac.ma>
2023-08-19 19:36:20 +0100

    combine signip and login tests

--- backend-app/tests/e2e/auth/login_signup.test.js
+++ backend-app/tests/e2e/auth/login_signup.test.js
@@ -1,8 +1,49 @@
 const request = require('supertest');
 const app = require('../../../app');
 const { testUserCredentials } = require('../../testConstants');
-const { REQUIRE_ACTIVATION } = require('../../../config/app_config');
 const { createUser, deleteUser } = require('../../utils');
+const { REQUIRE_ACTIVATION } = require('../../../config/app_config');
+
+describe('POST /api/auth/login', () => {
+    // console.log('db connected: ', mongoose.connection.readyState);
+
+    beforeAll(async () => {
+        await createUser(
+            app,
+            testUserCredentials.userName,
+            testUserCredentials.userEmail,
+            testUserCredentials.userPassword
+        );
+    });
+
+    it('should return a JWT token on successful login', async () => {
+        const response = await request(app)
+            .post('/api/auth/login')
+            .send({
+                email: testUserCredentials.userEmail,
+                password: testUserCredentials.userPassword,
+            })
+            .expect(200);
+        expect(response.body.accessToken).toBeDefined();
+    });
+
+    it('should return 401 if invalid credentials are provided', async () => {
+        await request(app)
+            .post('/api/auth/login')
+            .send({
+                email: 'random@gmail.com',
+                password: 'admin123424',
+            })
+            .expect(401);
+    });
+    afterAll(async () => {
+        await deleteUser(
+            app,
+            testUserCredentials.userEmail,
+            testUserCredentials.userPassword
+        );
+    });
+});
 
 describe('POST /api/auth/signup', () => {
     beforeAll(async () => {
