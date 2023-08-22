c9ca18f0c361e30db87ee430ad24a8c30d0cf19b
Abdelouahab Bella <a.bella4246@uca.ac.ma>
2023-08-22 13:04:11 +0100

    help wanted - disable signup

--- backend-app/tests/e2e/auth/login_signup.test.js
+++ backend-app/tests/e2e/auth/login_signup.test.js
@@ -54,25 +54,25 @@ describe('POST /api/auth/signup', () => {
         );
     });
 
-    it('should create a new user', async () => {
-        const response = await createUser(
-            app,
-            testUserCredentials.userName,
-            testUserCredentials.userEmail,
-            testUserCredentials.userPassword
-        );
-        expect(response.status).toBe(201);
-        expect(response.body.user).toBeDefined();
-        expect(response.body.user.name).toBe(testUserCredentials.userName);
-        expect(response.body.user.email).toBe(testUserCredentials.userEmail);
-        expect(response.body.user.roles).toEqual(
-            expect.arrayContaining(['USER'])
-        );
-        expect(response.body.accessToken).toBeDefined();
-        expect(response.body.user.active).toBe(!REQUIRE_ACTIVATION);
-        token = response.body.token;
-        userId = response.body.user._id;
-    });
+    // it('should create a new user', async () => {
+    //     const response = await createUser(
+    //         app,
+    //         testUserCredentials.userName,
+    //         testUserCredentials.userEmail,
+    //         testUserCredentials.userPassword
+    //     );
+    //     expect(response.status).toBe(201);
+    //     expect(response.body.user).toBeDefined();
+    //     expect(response.body.user.name).toBe(testUserCredentials.userName);
+    //     expect(response.body.user.email).toBe(testUserCredentials.userEmail);
+    //     expect(response.body.user.roles).toEqual(
+    //         expect.arrayContaining(['USER'])
+    //     );
+    //     expect(response.body.accessToken).toBeDefined();
+    //     expect(response.body.user.active).toBe(!REQUIRE_ACTIVATION);
+    //     token = response.body.token;
+    //     userId = response.body.user._id;
+    // });
 
     afterAll(async () => {
         await deleteUser(
